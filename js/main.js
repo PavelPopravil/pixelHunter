e8e3e833e094c001971e1afdf39adf4e40e04e8c
Shishic <pasukmc@gmail.com>
2018-10-28 14:24:36 +0300

    add screenChange function

--- js/main.js
+++ js/main.js
@@ -3,6 +3,13 @@
 // console.log(util);
 
 const screens = [...document.querySelectorAll('template')];
+
+const initialState = {
+    startScreen: 0,
+    currentScreen: 0,
+    screenLength: screens.length
+};
+
 const mainScreen = document.querySelector('.central');
 
 const showScreen = ((screen) => {
@@ -11,8 +18,21 @@ const showScreen = ((screen) => {
     mainScreen.appendChild(template);
 });
 
-showScreen(screens[0]);
-
 document.addEventListener('keydown', (e) => {
-    console.log(e.keyCode);
+
+    if (e.altKey && e.keyCode === 39) {
+        console.log('alt + ]');
+        if (initialState.currentScreen < initialState.screenLength - 1) {
+            showScreen(screens[++initialState.currentScreen]);
+        }
+
+    } else if (e.altKey && e.keyCode === 37) {
+        console.log('alt + [');
+        if (initialState.currentScreen > 0) {
+            showScreen(screens[--initialState.currentScreen]);
+        }
+    
+    }
 });
+
+showScreen(screens[initialState.startScreen]);
